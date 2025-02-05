f9987eee4da3c0e36201d0025778ae5b1641ae1c
Saikiran131-Developer <saikiranchary131@gmail.com>
2025-02-05 10:33:26 +0530

    Add files via upload

---
+++ controlstatements.py
@@ -0,0 +1,21 @@
+#1
+num=1
+while num<=100:
+    print(num)
+    num+=1
+#2.1
+for i in range (0,100,-1):
+    print(i)
+#2.2    
+num1=100
+while num1>=1:
+    print(num1)
+    num1-=1
+
+#3
+i=1234
+rev=0
+while(i>0):
+    rev=(rev*10)+i%10
+    i=i//10
+print(rev)
